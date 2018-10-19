55ecadee7f58897c7b96b7771370bcc9878dac90
malaligowda <mg.genomics@gmail.com>
2018-10-19 10:50:28 +0530

    Initial commit

---
+++ app/src/main/java/com/example/malaligowda/billreminder/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.malaligowda.billreminder;
+
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toolbar;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+     private TextView timeView;
+     private TextView dayNumber;
+     private TextView month;
+     private TextView year;
+     private TextView day;
+     private RecyclerView displayBill;
+     private ImageButton addBill;
+     private RadioButton mainBill;
+     private RadioButton mainSubscription;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        dayNumber = findViewById(R.id.dayNumberText);
+        month = findViewById(R.id.monthText);
+        year = findViewById(R.id.yearText);
+        day = findViewById(R.id.dayText);
+        displayBill = findViewById(R.id.displayView);
+        addBill = findViewById(R.id.addPaymentButton);
+        mainBill = findViewById(R.id.mainBillButton);
+        mainSubscription = findViewById(R.id.mainSubscriptionButton);
+
+
+
+
+
+    }
+
+
+}
+
+
---
+++ app/src/main/java/com/example/malaligowda/billreminder/addActivity.java
@@ -0,0 +1,60 @@
+package com.example.malaligowda.billreminder;
+
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+public class addActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
+
+    private EditText titleView;
+    private EditText amountView;
+    private CalendarView mCalendarView;
+    private Spinner currrenySpinner;
+    private Spinner ringtoneSpinner;
+    private Spinner intervalSpinner;
+    private CheckBox reminder;
+    private TextView alarmTime;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        titleView = findViewById(R.id.titleView);
+        amountView = findViewById(R.id.amountView);
+        mCalendarView = findViewById(R.id.calendarView);
+        currrenySpinner = findViewById(R.id.currencySpinner);
+        ringtoneSpinner = findViewById(R.id.ringView);
+        intervalSpinner = findViewById(R.id.intervalView);
+        reminder = findViewById(R.id.checkBox);
+        alarmTime = findViewById(R.id.alarmTimeView);
+
+        alarmTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                DialogFragment timePicker = new TimePickerFragmant();
+                timePicker.show(getSupportFragmentManager(), "Time Picker");
+
+            }
+        });
+
+
+
+
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        alarmTime.setText(hourOfDay+":"+minute);
+    }
+}
