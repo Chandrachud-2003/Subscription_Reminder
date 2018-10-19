8e5c035103bf37c27b82e1ad71204eef5b35a443
rohitkanagal <44258377+rohitkanagal@users.noreply.github.com>
2018-10-19 16:09:27 +0530

    test-2

--- app/src/main/java/com/example/malaligowda/billreminder/addActivity.java
+++ app/src/main/java/com/example/malaligowda/billreminder/addActivity.java
@@ -1,56 +1,90 @@
 package com.example.malaligowda.billreminder;
 
 import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
-public  class addActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
+import java.text.DateFormat;
+import java.util.Date;
+
+public abstract class addActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
 
     private EditText titleView;
     private EditText amountView;
     private CalendarView mCalendarView;
-    private Spinner currrenySpinner;
+    private Spinner currencySpinner;
     private Spinner ringtoneSpinner;
     private Spinner intervalSpinner;
     private CheckBox reminder;
     private TextView alarmTime;
-
+    private ImageButton backbutton;
+    private Button addbutton;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.add_bill);
 
         titleView = findViewById(R.id.titleView);
         amountView = findViewById(R.id.amountView);
         mCalendarView = findViewById(R.id.calendarView);
-        currrenySpinner = findViewById(R.id.currencySpinner);
+        currencySpinner = findViewById(R.id.currencySpinner);
         ringtoneSpinner = findViewById(R.id.ringView);
         intervalSpinner = findViewById(R.id.intervalView);
         reminder = findViewById(R.id.checkBox);
         alarmTime = findViewById(R.id.alarmTimeView);
+        backbutton = findViewById(R.id.backButton);
+        addbutton = findViewById(R.id.addPaymentButton);
+        String[] intervalarraySpinner = new String[] {"Monthly", "Annually"};
+        String[] currencyarraySpinner = new String[] {"AUD $","EUR €", "INR ₹","USD $","JPY	¥","ZAR R"};
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, intervalarraySpinner);
+        intervalSpinner.setAdapter(adapter1);
+
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, currencyarraySpinner);
+        currencySpinner.setAdapter(adapter2);
+            mCalendarView.setDate(System.currentTimeMillis(),false,true);
 
 
-        alarmTime.setOnClickListener(new View.OnClickListener() {
+
+        backbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                DialogFragment timePicker = new TimePickerFragmant();
-                timePicker.show(getSupportFragmentManager(), "Time Picker");
+                Intent intent = new Intent(addActivity.this, MainActivity.class);
+                startActivity(intent);
 
             }
         });
+        addbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+                    @Override
+                    public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                        int billyear = year;
+                        int billmonth = month;
+                        int billday = dayOfMonth;
+                    }
+                });
 
+            }
 
+        });
 
 
 
