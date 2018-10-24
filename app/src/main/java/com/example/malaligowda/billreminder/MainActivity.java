b60cd53509abf84bfd6607e837f84a7b9d53a381
rohitkanagal <44258377+rohitkanagal@users.noreply.github.com>
2018-10-24 17:50:45 +0530

    test-14

--- app/src/main/java/com/example/malaligowda/billreminder/MainActivity.java
+++ app/src/main/java/com/example/malaligowda/billreminder/MainActivity.java
@@ -10,6 +10,7 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.helper.ItemTouchHelper;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.RadioButton;
@@ -17,9 +18,12 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 
@@ -58,10 +62,8 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
 
 
-
             }
         });
-
 
 
         mDBHandler = new MyDBHandler(this, null, null, 5);
@@ -74,34 +76,58 @@ public class MainActivity extends AppCompatActivity {
         Date d = new Date();
         String dayOfTheWeek = sdf.format(d);
         day.setText(dayOfTheWeek);
-        String month1="";
+        String month1 = "";
         SimpleDateFormat sdf1 = new SimpleDateFormat("dd/MM/yyyy");
         String datee = sdf1.format(new Date());
         switch (Calendar.getInstance().get(Calendar.MONTH)) {
-            case 0:{ month1 = "JAN";
-                break;}
-            case 1:{ month1 = "FEB";
-                break;}
-            case 2:{ month1 = "MAR";
-                break;}
-            case 3:{ month1 = "APR";
-                break;}
-            case 5:{ month1 = "JUN";
-                break;}
-            case 6:{ month1 = "JUL";
-                break;}
-            case 7:{ month1 = "AUG";
-                break;}
-            case 8:{ month1 = "SEP";
-                break;}
-            case 9:{ month1 = "OCT";
-                break;}
-            case 10:{ month1 = "NOV";
-                break;}
-            case 11:{ month1 = "DEC";
-                break;}
-            case 4:{ month1 = "MAYY";
-                break;}
+            case 0: {
+                month1 = "JAN";
+                break;
+            }
+            case 1: {
+                month1 = "FEB";
+                break;
+            }
+            case 2: {
+                month1 = "MAR";
+                break;
+            }
+            case 3: {
+                month1 = "APR";
+                break;
+            }
+            case 5: {
+                month1 = "JUN";
+                break;
+            }
+            case 6: {
+                month1 = "JUL";
+                break;
+            }
+            case 7: {
+                month1 = "AUG";
+                break;
+            }
+            case 8: {
+                month1 = "SEP";
+                break;
+            }
+            case 9: {
+                month1 = "OCT";
+                break;
+            }
+            case 10: {
+                month1 = "NOV";
+                break;
+            }
+            case 11: {
+                month1 = "DEC";
+                break;
+            }
+            case 4: {
+                month1 = "MAYY";
+                break;
+            }
         }
         String year1 = String.valueOf(Calendar.getInstance().get(Calendar.YEAR));
         year.setText(year1);
@@ -124,61 +150,125 @@ public class MainActivity extends AppCompatActivity {
         ArrayList<String> mId;
         ArrayList<String> mSync;
 
-
-
-
-
+        ArrayList<Date> ndate = new ArrayList<>();
 
 
         mId = mDBHandler.idArray();
         mNames = mDBHandler.namesArray();
-            mDate = mDBHandler.dateArray();
-            mCurrency = mDBHandler.currencyArray();
-            mAmount = mDBHandler.amountArray();
-            mInterval = mDBHandler.intervalArray();
-            mNotify = mDBHandler.notifyArray();
-            mType = mDBHandler.typeArray();
-            mNotifyDays = mDBHandler.notifyDays();
-            mSync = mDBHandler.syncArray();
+        mDate = mDBHandler.dateArray();
+        mCurrency = mDBHandler.currencyArray();
+        mAmount = mDBHandler.amountArray();
+        mInterval = mDBHandler.intervalArray();
+        mNotify = mDBHandler.notifyArray();
+        mType = mDBHandler.typeArray();
+        mNotifyDays = mDBHandler.notifyDays();
+        mSync = mDBHandler.syncArray();
 
-        if (mNames.size()!=0)
-        {
+        if (mNames.size() != 0) {
+     //SORTING
+            SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+            try {
+                for (int i = 0; i < mDate.size(); i++)
+                    ndate.add(format.parse(mDate.get(i)));
+
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            int min;
+            for (int i = 0; i < mDate.size(); i++)
+            {
+                min = i;
+                for(int j = i+1;j<mDate.size();j++) {
+                    if (ndate.get(j).before(ndate.get(i))) {
+
+                        min=j;
+                    }
+                }
+                Date temp = ndate.get(i);
+                ndate.set(i, ndate.get(min));
+                ndate.set(min, temp);
+
+
+                String namet = mNames.get(i);
+                mNames.set(i, mNames.get(min));
+                mNames.set(min, namet);
+
+
+                String idt = mId.get(i);
+                mId.set(i, mId.get(min));
+                mId.set(min, idt);
+
+
+                String currencyt = mCurrency.get(i);
+                mCurrency.set(i, mCurrency.get(min));
+                mCurrency.set(min, currencyt);
+
+
+                String amount = mAmount.get(i);
+                mAmount.set(i, mAmount.get(min));
+                mAmount.set(min, amount);
+
+
+                String interval  = mInterval.get(i);
+                mInterval.set(i, mInterval.get(min));
+                mInterval.set(min, interval);
+
+
+                String notify  = mNotify.get(i);
+                mNotify.set(i, mNotify.get(min));
+                mNotify.set(min, notify);
+
+
+                String type  = mType.get(i);
+                mType.set(i, mType.get(min));
+                mType.set(min, type);
+
+
+                String notifydays  = mNotifyDays.get(i);
+                mNotifyDays.set(i, mNotifyDays.get(min));
+                mNotifyDays.set(min, notifydays);
+
+                
+                String sync  = mSync.get(i);
+                mSync.set(i, mSync.get(min));
+                mSync.set(min, sync);
+
+
+
+            }
+
+            for (int i = 0; i < mDate.size(); i++) {
+                Date date = ndate.get(i);
+                String dateTime = format.format(date);
+                mDate.set(i,dateTime);
+            }
 
 
 
 
-            displayAdapter displayAdapter = new displayAdapter(this,mId, mNames, mDate, mCurrency, mAmount, mType, mNotify, mInterval, mNotifyDays, mSync);
+
+
+            Log.d("rohit",ndate.toString() );
+            Log.d("rohit",mNames.toString() );
+            displayAdapter displayAdapter = new displayAdapter(this, mId, mNames, mDate, mCurrency, mAmount, mType, mNotify, mInterval, mNotifyDays, mSync);
             displayBills.setVisibility(View.VISIBLE);
             StaggeredGridLayoutManager staggeredGridLayoutManager = new StaggeredGridLayoutManager(NUM_COLUMNS, LinearLayoutManager.VERTICAL);
             displayBills.setLayoutManager(staggeredGridLayoutManager);
             displayBills.setAdapter(displayAdapter);
 
 
+        } else
+
+        {
+
+            Toast.makeText(getBaseContext(), "No Reminders Set", Toast.LENGTH_SHORT).show();
+            displayBills.removeAllViewsInLayout();
+            displayBills.setAdapter(null);
 
         }
-
-
-     else
-
-    {
-
-        Toast.makeText(getBaseContext(), "No Reminders Set", Toast.LENGTH_SHORT).show();
-        displayBills.removeAllViewsInLayout();
-        displayBills.setAdapter(null);
-
     }
+
 }
-
-    public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction, int position) {
-        if(viewHolder instanceof displayAdapter.ViewHolder) {
-            int deleteIndex = viewHolder.getAdapterPosition();
-
-
-        }
-
-    }
-}
-
 
 
 
